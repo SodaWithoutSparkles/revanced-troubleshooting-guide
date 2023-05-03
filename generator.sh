c8e58d72bf863b12a7f37fa7a59a49e30294f62d
SodaWithoutSparkles <64138578+SodaWithoutSparkles@users.noreply.github.com>
2023-05-04 00:38:01 +0800

    refactor: readability

--- generator.sh
+++ generator.sh
@@ -6,7 +6,12 @@ if [[ "$1" == *"revanced-troubleshooting-guide"* ]]; then
     read -p "Are you sure? " -n 1 -r
     echo    # (optional) move to a new line
     if [[ $REPLY =~ ^[Yy]$ ]]; then
-        echo '' > ../${PWD##*/}.md && for i in *.md; do cat "$i" >> ../${PWD##*/}.md; echo -n '#' >> ../${PWD##*/}.md; done && truncate -s -1 ../${PWD##*/}.md
+        rm ../${PWD##*/}.md
+        for i in *.md; do
+            cat "$i" >> ../${PWD##*/}.md;
+            echo -n '#' >> ../${PWD##*/}.md;
+        done
+        truncate -s -1 ../${PWD##*/}.md
     else
         echo 'abort'
     fi
