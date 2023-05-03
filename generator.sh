24a4293ff24d8ea371e292223871192439c07547
SodaWithoutSparkles <64138578+SodaWithoutSparkles@users.noreply.github.com>
2023-05-04 01:10:54 +0800

    feat: remove empty new lines

--- generator.sh
+++ generator.sh
@@ -6,12 +6,15 @@ if [[ "$1" == *"revanced-troubleshooting-guide"* ]]; then
     read -p "Are you sure? " -n 1 -r
     echo    # (optional) move to a new line
     if [[ $REPLY =~ ^[Yy]$ ]]; then
-        rm ../${PWD##*/}.md
+        rm -f ../${PWD##*/}.md
         for i in *.md; do
             cat -s "$i" >> ../${PWD##*/}.md;
+            echo '' >> ../${PWD##*/}.md;
             echo -n '#' >> ../${PWD##*/}.md;
         done
+        sed -i '/\S/,/^\s*$/!d' ../${PWD##*/}.md;
         truncate -s -1 ../${PWD##*/}.md
+        sed -i '/\S/,/^\s*$/!d' ../${PWD##*/}.md;
     else
         echo 'abort'
     fi
