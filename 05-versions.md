104c2590d7ff400a36dcbe5799ff4cc482532a03
SodaWithoutSparkles <64138578+SodaWithoutSparkles@users.noreply.github.com>
2023-05-18 21:22:19 +0800

    feat: add python version checker

---
+++ 05-versions.md
@@ -0,0 +1,40 @@
+# Versions to use
+
+This is a list of versions to use with ReVanced patches:
+
+| Package Name | Version | Common Name |
+|---|---|---|
+| com.google.android.youtube | 18.16.37 | YouTube |
+| com.google.android.apps.youtube.music | 5.39.52 | YouTube Music |
+| com.ss.android.ugc.trill | 27.8.3 | TikTok |
+| tv.twitch.android.app | 14.6.1 | Twitch |
+| com.twitter.android | 9.71.0-release.0 | Twitter |
+| com.reddit.frontpage | 2023.12.0 | Reddit |
+| com.spotify.music | ANY | Spotify |
+| at.gv.bmf.bmf2go | 2.2.0 | FinanzOnline |
+| at.gv.oe.app | ANY | Digitales Amt |
+| co.windyapp.android | ANY | Blue Windy |
+| com.awedea.nyx | ANY | Nyx Music Player |
+| com.backdrops.wallpapers | 4.52 | Backdrops - Wallpapers |
+| com.dci.dev.androidtwelvewidgets | ANY | Android 12 Widgets (Twelve) |
+| com.facebook.orca | ANY | Facebook Messenger |
+| com.instagram.android | ANY | Instagram |
+| com.ithebk.expensemanager | ANY | Money Manager - Expense, Budget |
+| com.microblink.photomath | 8.21.0 | Photomath |
+| com.myprog.hexedit | ANY | Hex Editor |
+| com.nis.app | ANY | Inshorts |
+| com.spotify.lite | ANY | Spotify lite |
+| com.teslacoilsw.launcher | ANY | Nova Launcher |
+| com.ticktick.task | ANY | TickTick: To-do list & Tasks |
+| com.vanced.android.youtube | ANY | old Vanced |
+| com.zombodroid.MemeGenerator | 4.6364 | Meme Generator |
+| de.dwd.warnapp | ANY | WarnWetter |
+| eu.faircode.netguard | ANY | NetGuard - no-root firewall |
+| ginlemon.iconpackstudio | ANY | Icon Pack Studio |
+| io.yuka.android | ANY | Yuka - Food & cosmetic scan |
+| net.binarymode.android.irplus | ANY | irplus - Infrared Remote |
+| net.dinglisch.android.taskerm | ANY | Tasker |
+| org.citra.citra_emu | ANY | Citra - Nintendo 3DS Emulator |
+| org.totschnig.myexpenses | 3.4.9 | My Expenses |
+
+Last Update: 2023-05-18 13:13:11 UTC
---
+++ checkReVancedVersion.py
@@ -0,0 +1,231 @@
+#!/usr/bin/python3
+
+import requests
+import json
+from datetime import datetime
+from packaging import version
+import os
+import logging
+
+JSON_FILE = '.conf/python/update.ver.json'
+STATE_FILE = '.conf/python/update.state'  # for last update
+LUT_FILE = '.conf/python/update.lut.json'  # lookup table, pkgname to common name
+TEMPLATE_FILE = '.conf/python/update.template'
+OUT_FILE = '05-versions.md'
+
+if os.getenv('GITHUB_ACTIONS') != 'true':
+    EXPORT = None
+    if os.getenv('FORCE') != None:
+        FORCED = True
+    else:
+        FORCED = False
+else:
+    EXPORT = os.getenv('GITHUB_OUTPUT')
+    if os.getenv('EVENT') == 'workflow_dispatch':
+        # manually run, force update
+        FORCED = True
+    else:
+        FORCED = False
+
+API = 'https://releases.revanced.app/patches'
+
+logging.basicConfig(level=logging.INFO,
+                    format='%(asctime)s.%(msecs)03d: [%(levelname)s] %(message)s',
+                    datefmt='%d-%b-%y %H:%M:%S')
+
+modified = False
+
+def compVer(ver1: str, ver2: str):
+    # 1 if ver1 > ver2
+    # 0 if ver1 = ver2
+    # -1 if ver1 < ver2
+    # ANY should be ranked last
+    # oh wait i know why
+    # i am finding the LOWEST of the comp
+    # so any should be ranked FIRST
+    # print(ver1, ver2, end='')
+    if (ver1 == ver2) and (ver1 == 'ANY'):
+        return 0
+    if ver1 == 'ANY':
+        # print(ver1, ver2)
+        return 1
+    if ver2 == 'ANY':
+        return 0
+    try:
+        # semantic version compare
+        pver1 = version.parse(ver1)
+        pver2 = version.parse(ver2)
+        if pver1 > pver2:
+            return 1
+        elif pver1 < pver2:
+            return -1
+        return 0
+    except:
+        # strcmp
+        if ver1 > ver2:
+            return 1
+        elif ver1 < ver2:
+            return -1
+        return 0
+
+
+def findVer(versions: list):
+    # Find Latest Supported Version (LSV)
+    # input **SORTED** list of {'pkgName': pkgName, 'ver': LSV}
+    # return list of {'pkgName': pkgName, 'ver': LSV, 'count': count} ...?
+
+    # init 3 parallel arrays
+    pkgName = []
+    lsv = []
+    patchCount = []
+
+    curr = ''
+    # checking if we hit another pkg, start a new entry in parallel arrays
+    for item in versions:
+        # new pkg?
+        next = False
+        if item['pkgName'] != curr:
+            curr = item['pkgName']
+            pkgName.append(curr)
+            next = True
+            patchCount.append(0)
+            logging.debug(f'Found new pkgName {curr}')
+        if next:
+            lsv.append(item['ver'])
+            logging.debug(f'Set {curr} version to {lsv[-1]}')
+        patchCount[-1] += 1
+        # if latest supported > current
+        if compVer(lsv[-1], item['ver']) == 1:
+            # change lsv to current
+            logging.debug(
+                f'Bump {curr} version from {lsv[-1]} to {item["ver"]}')
+            lsv[-1] = item['ver']
+
+    out = []
+    for i, name in enumerate(pkgName):
+        out.append({'pkgName': name, 'ver': lsv[i], 'count': patchCount[i]})
+
+    return out
+
+
+def getJson():
+    # get json obj and save to JSON_PATH
+    logging.info('Requesting API...')
+    data = requests.get(API).json()
+    with open(JSON_FILE, 'w') as f:
+        json.dump(data, f)
+    return
+
+
+def parseJson():
+    # return list of sorted {'pkgName': pkgName, 'ver': maxVer}, extracted from json
+    # note: this rely on API to ensure the maxVer is ordered last
+    logging.info('Parsing response...')
+    rawJson = ''
+    with open(JSON_FILE, 'r') as f:
+        rawJson = json.loads(f.read())
+    out = []
+    for item in rawJson:
+        try:
+            try:
+                # TODO: parse and get max semantic ver, fail to string comp
+                maxVer = item['compatiblePackages'][0]['versions'][-1]
+            except:
+                # support all versions
+                maxVer = 'ANY'
+            pkgName = item['compatiblePackages'][0]['name']
+            out.append({'pkgName': pkgName, 'ver': maxVer})
+        except IndexError:
+            # universal patches w/o pkgName or ver
+            # ignore them
+            pass
+        except Exception as e:
+            logging.critical(e)
+
+    return sorted(out, key=lambda d: d['pkgName'])
+
+
+def fillTemplate(lsv: list):
+    logging.info('Filling template...')
+    rawJson = ''
+    table = []
+    with open(LUT_FILE, 'r') as f:
+        rawJson = json.loads(f.read())
+    table.append('| Package Name | Version | Common Name |\n')
+    table.append('|---|---|---|\n')
+    for item in lsv:
+        try:
+            commonName = rawJson[item['pkgName']][0]
+        except KeyError:
+            commonName = '_unknown_'
+            logging.warning(
+                f'Unable to find common name for {item["pkgName"]}, using _unknown_ instead'
+            )
+        except Exception as e:
+            logging.critical(e)
+
+        table.append(f"| {item['pkgName']} | {item['ver']} | {commonName} |\n")
+
+    with open(TEMPLATE_FILE, "r") as t:
+        # contents = t.readlines()
+        with open(OUT_FILE, 'w') as f:
+            for line in t:
+                if line == '{table}\n':
+                    f.writelines(table)
+                elif line == '{date}\n':
+                    timestring = datetime.utcnow().isoformat(
+                        sep=" ", timespec="seconds") + ' UTC'
+                    logging.info(f'Set last update timestamp to {timestring}')
+                    f.write(f'Last Update: {timestring}\n')
+                else:
+                    f.write(line)
+
+
+
+def exportOutput(key:str, value:str):
+    # export the key:value pair to gh action output
+    if EXPORT != None:
+        print(f'{key}={value}', file=EXPORT)
+    return
+
+
+def main():
+    global modified
+    getJson()
+    with open(STATE_FILE, 'r') as f:
+        lastUpdated = int(f.readline())
+        lastResult = f.readline()
+    # lastUpdated = 1
+    # lastResult = ''
+
+    result = sorted(findVer(parseJson()),
+                    key=lambda d: d['count'],
+                    reverse=True)
+    timeNow = int(datetime.utcnow().strftime('%s'))
+    
+    if str(result) != lastResult:
+        modified = True
+        logging.info('Patches info updated, modifing state file')
+
+    # not modified within 1 week
+    if (timeNow - lastUpdated) > 604800:
+        logging.info('Last update over 1 week ago, bumping timestamp')
+        modified = True
+
+    if modified or FORCED:
+        with open(STATE_FILE, 'w') as f:
+            f.write(str(timeNow)+'\n')
+            f.write(str(result))
+        exportOutput('modified', 'true')
+        logging.info(f'Need to update output, {modified=}, {FORCED=}')
+        fillTemplate(result)
+    else:
+        exportOutput('modified', 'false')
+        logging.info('No changes made, no need to update')
+    logging.info('Done')
+
+if __name__ == '__main__':
+    main()
+# print(json.dumps(result))
+
+# findVer('com.google.android.youtube', )
